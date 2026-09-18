9764536968e1bc007b38373f9390c3b7cd51953c
kotsiavrasalexandros <m99h4d96b5@privaterelay.appleid.com>
2026-09-18 14:02:49 -0600

    Add files via upload

--- vanityplates.py
+++ vanityplates.py
@@ -1,3 +1,4 @@
+from collections import Counter
 def main():
     plate = input("Plate: ")
     if is_valid(plate):
@@ -8,9 +9,13 @@ def main():
 
 def is_valid(plate):
     if len(plate) >= 2 and len(plate) <= 6:
-        return True
-    else:
+        pass
+    if plate[1] == "0":
         return False
+    if plate[1].isalpha() and plate[2].isdigit():
+        return False
+    elif len(plate) >= 2 and len(plate) <= 6:
+        return True
 
 
 main()
